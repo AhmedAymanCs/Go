c9c6b965786fa6be5ed9cb631f11d895ceb7ff51
Ahmed <benzemaa46@gmail.com>
2026-03-16 14:25:03 +0200

    enhance home ui

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,4 @@
 class AppConstants {
-  static const String databaseCollectionName = 'Texts';
   static const String usersCollectionName = 'Users';
   static const String userSession = 'user_session';
   static const String dateTimeFormat = 'dd MMM yyyy - hh:mm a';
--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:go/core/constants/color_manager.dart';
 import 'package:go/core/widgets/cutom_form_field.dart';
 import 'package:go/core/widgets/logo_with_text.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _destinationController;
+  @override
+  void initState() {
+    super.initState();
+    _destinationController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _destinationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,27 +30,42 @@ class HomePage extends StatelessWidget {
       body: Stack(
         children: [
           const Placeholder(),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(20),
+          DraggableScrollableSheet(
+            initialChildSize: 0.5,
+            minChildSize: 0.25,
+            maxChildSize: 0.9,
+            builder: (context, scrollController) {
+              return Container(
+                padding: const EdgeInsets.all(16),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
                 ),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  LogoWithText(text: 'Go'),
-                  const SizedBox(height: 8),
-                  CustomFormField(hint: 'Where do you want to go?'),
-                ],
-              ),
-            ),
+                child: SingleChildScrollView(
+                  controller: scrollController,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 40,
+                        height: 4,
+                        margin: const EdgeInsets.only(bottom: 12),
+                        decoration: BoxDecoration(
+                          color: ColorManager.gray500,
+                          borderRadius: BorderRadius.circular(2),
+                        ),
+                      ),
+                      LogoWithText(text: ''),
+                      const SizedBox(height: 8),
+                      CustomFormField(
+                        hint: 'Where do you want to go?',
+                        controller: _destinationController,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ],
       ),
